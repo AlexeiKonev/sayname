8a10ae554b8ef47d39987aad1ca0c8f5b1b3a902
AlexeiKonev <alexkonev777@gmail.com>
2024-03-24 13:56:21 +0300

    gravitation switch

---
+++ Assets/Scripts/Cube Game/GravitationManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationManager : MonoBehaviour {
+
+    public Rigidbody[] cubesBody;
+
+    public void SwitchGravitation(bool state)
+    {
+        foreach(var r in cubesBody)
+        {
+            r.useGravity = state;
+        }
+    }
+}
